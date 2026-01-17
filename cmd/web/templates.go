5c7a0b2a743008d73176c359df0aff5ac2d4aa17
Ryan Murphy <3978422+Rymurphy12@users.noreply.github.com>
2026-01-17 10:29:37 -0600

    Finished up to CH 5.2

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/rymurphy12/snippetbox/internals/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
